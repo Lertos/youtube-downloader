7501f85fe5c8850dbaf2e1942b838ec20c655b5d
Lertos <deetasticgaming@gmail.com>
2023-01-13 08:51:00 -0400

    Adding song list class to keep song data organized

---
+++ src/main/java/com/lertos/youtubedownloader/SongList.java
@@ -0,0 +1,38 @@
+package com.lertos.youtubedownloader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SongList {
+
+    private List<Song> songs;
+    private int currentIndex;
+
+    public SongList() {
+        this.songs = new ArrayList<>();
+        this.currentIndex = 0;
+    }
+
+    public List<Song> getSongs() {
+        return Collections.unmodifiableList(songs);
+    }
+
+    public void removeSong(int index) {
+        for (Song song : songs) {
+            if (song.getIndex() == index)
+                songs.remove(index);
+        }
+    }
+
+    public int addSong(String videoTitle, String URL) {
+        Song song = new Song(currentIndex, videoTitle, URL);
+        int index = currentIndex;
+
+        songs.add(song);
+        currentIndex++;
+
+        return index;
+    }
+
+}
