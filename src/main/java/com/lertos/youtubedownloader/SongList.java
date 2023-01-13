3abd95fe2071383e4f868e7d545ff68b85ef21b5
Lertos <deetasticgaming@gmail.com>
2023-01-13 09:02:54 -0400

    Making Song an inner class as it isn't needed elsewhere

--- src/main/java/com/lertos/youtubedownloader/SongList.java
+++ src/main/java/com/lertos/youtubedownloader/SongList.java
@@ -35,4 +35,29 @@ public class SongList {
         return index;
     }
 
+    private class Song {
+
+        private final int index;
+        private final String videoTitle;
+        private final String URL;
+
+        public Song(int index, String videoTitle, String URL) {
+            this.index = index;
+            this.videoTitle = videoTitle;
+            this.URL = URL;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public String getVideoTitle() {
+            return videoTitle;
+        }
+
+        public String getURL() {
+            return URL;
+        }
+    }
+
 }
